0231ac4c1ef3d10d7781a53e4969b577bf87b9c7
Max Gunko <smouge1989@mail.ru>
2023-12-18 00:30:45 +0500

    fix: fixed error serializable

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,6 +3,9 @@ import { reducer } from './ducks'
 
 const store = configureStore({
   reducer: reducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: false
+  }),
 })
 export type RootState = ReturnType<typeof reducer>
 
